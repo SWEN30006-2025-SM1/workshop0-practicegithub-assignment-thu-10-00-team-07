d541a5abd06b1dd9c28738ebbf0cf214e4e0096a
jax ! <jaxonakhyar@gmail.com>
2025-03-06 10:58:32 +1100

    implemented calculator methods

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -2,22 +2,25 @@ package example;
 
 public class Calculator {
     public int add(int a, int b) {
-        // TODO: Implement this method
-        return 4;
+        return a + b;
     }
 
     public int minus(int a, int b) {
-        // TODO: Implement this method
-        return 10;
+        return a - b;
     }
 
     public int multiply(int a, int b) {
-        // TODO: Implement this method
-        return 10;
+        return a * b;
     }
 
     public double divide(int a, int b) {
-        // TODO: Implement this method
-        return 5;
+        try {
+            return (double) a / b;
+        }
+        catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+
     }
 }
